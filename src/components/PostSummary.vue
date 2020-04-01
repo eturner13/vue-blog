68abc8e0de5454378b4ca7fbd7e2ca134fed9265
unknown <planetbutler@gmail.com>
2020-04-01 15:37:45 -0400

    trying to wire up post page

--- src/components/PostSummary.vue
+++ src/components/PostSummary.vue
@@ -5,13 +5,20 @@
         <div class="author">{{ post.author }}</div>
         <div class="title">{{ post.title }}</div>
         <div class="body">{{ post.body }}</div>
+        <button @click="goToPost">Go to Post</button>
     </div>
 </template>
 
 <script>
+
 export default {
     name: 'PostSummary',
-    props: ['post']
+    props: ['post'],
+    methods: {
+        goToPost() {
+            this.$router.push('/post/:id')
+        },
+    }
 }
 </script>
 
---
+++ src/views/Post.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="post">
+        <div class="header">
+            <div v-for="post in posts" :key="post.id">
+                <PostSummary :post="post"/>
+            </div>
+            <button @click="backToList">Back to List</button>    
+        </div>  
+    </div>
+</template>
+
+<script>
+import PostSummary from '@/components/PostSummary'
+import { mapState } from 'vuex'
+
+export default {
+    name: 'Post',
+    components: { PostSummary },
+    computed: {
+        ...mapState(['posts'])
+    },
+    methods: {
+        backToList() {
+            this.$router.push({ name: 'list' });
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    .header {
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: space-between;
+        align-items: center;
+        margin: 20px;
+    }
+</style>
